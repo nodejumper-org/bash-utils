b5b5a7553219bb235ff1bbf1d595d77465c20ba8
libra422 <100301930+libra422@users.noreply.github.com>
2022-04-20 21:30:40 +0700

    add pkcs password and output dir arguments

--- ssl/install_cert_renewal_hook.sh
+++ ssl/install_cert_renewal_hook.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-while getopts u:d:s:p: flag
+while getopts u:d:s:p:l: flag
 do
     case "${flag}" in
         u) TARGET_USER=$OPTARG;;
         d) DOMAIN=$OPTARG;;
         s) SERVICES+=("$OPTARG");;
-        p) PKS12=true;;
+        p) PKCS12=true;;
+        r) PKCS12_PASS=$OPTARG
+        l) OUTPUT_DIR=$OPTARG;;
     esac
 done
 
@@ -24,27 +26,36 @@ if [ -z "$SERVICES" ]; then
     exit 1
 fi
 
-if [ -z "$PKS12" ]; then
-    PKS12=false
+if [ -z "$PKCS12" ]; then
+    PKCS12=false
+else
+    if [ "$PKCS12" = true ] && [ -z "$PKCS12_PASS" ]; then
+        echo "ERROR: pkcs12 password not provided"
+        exit 1
+    fi
 fi
 
-mkdir -p "$(eval echo ~$USER)/.ssl"
+if [ -z "$OUTPUT_DIR" ]; then
+    OUTPUT_DIR="/opt/ssl"
+fi
+
+mkdir -p "OUTPUT_DIR"
 
 sudo tee /etc/letsencrypt/renewal-hooks/deploy/apply_new_certs.sh > /dev/null <<EOF
 CERTS_DIR="/etc/letsencrypt/live/$DOMAIN"
-NEW_CERTS_DIR="$(eval echo ~$USER)/.ssl"
 TARGET_USER="$TARGET_USER"
 USER_GROUP="$(id -ng $TARGET_USER)"
 SERVICES=($(IFS=$' '; echo "${SERVICES[*]}"))
-PKS12=$PKS12
+PKCS12=$PKCS12
+PKCS12_PASS=$PKCS12_PASS
 
-sudo cp "\$CERTS_DIR/fullchain.pem" "\$NEW_CERTS_DIR/server_cert.pem"
-sudo cp "\$CERTS_DIR/privkey.pem" "\$NEW_CERTS_DIR/server_key.pem"
-sudo chown \$TARGET_USER:\$USER_GROUP "\$NEW_CERTS_DIR/server_cert.pem"
-sudo chown \$TARGET_USER:\$USER_GROUP "\$NEW_CERTS_DIR/server_key.pem"
+sudo cp "\$CERTS_DIR/fullchain.pem" "\$OUTPUT_DIR/server_cert.pem"
+sudo cp "\$CERTS_DIR/privkey.pem" "\$OUTPUT_DIR/server_key.pem"
+sudo chown \$TARGET_USER:\$USER_GROUP "\$OUTPUT_DIR/server_cert.pem"
+sudo chown \$TARGET_USER:\$USER_GROUP "\$OUTPUT_DIR/server_key.pem"
 
-if [ \$PKS12 == "true" ]; then
-    openssl pkcs12 -export -out \$NEW_CERTS_DIR/keystore.p12 -in \$NEW_CERTS_DIR/server_cert.pem -inkey \$NEW_CERTS_DIR/server_key.pem
+if [ \$PKCS12 == "true" ]; then
+    openssl pkcs12 -export -out \$OUTPUT_DIR/keystore.p12 -passout pass:pkcs12 \$PKCS12_PASS -in \$OUTPUT_DIR/server_cert.pem -inkey \$OUTPUT_DIR/server_key.pem
 fi
 
 for service in "\${SERVICES[@]}"; do
@@ -55,4 +66,4 @@ EOF
 
 chmod 750 /etc/letsencrypt/renewal-hooks/deploy/apply_new_certs.sh
 
-echo "Installed for User: $TARGET_USER; Domain: $DOMAIN; After the certs renew, these services will be restarted: ${SERVICES[*]}; Will a PKS12 key be generated: $PKS12"
+echo "Installed for User: $TARGET_USER; Domain: $DOMAIN; After the certs renew, these services will be restarted: ${SERVICES[*]}; Will a PKCS12 key be generated: $PKCS12"
